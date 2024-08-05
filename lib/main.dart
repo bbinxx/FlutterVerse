1710bcf218bbb2690427fccb771c156d109abc12
Bibin <bibinraju541@gmail.com>
2024-08-05 21:19:03 +0530

    view file location fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/views/home.dart';
+import 'package:FlutterVerse/views/home.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
